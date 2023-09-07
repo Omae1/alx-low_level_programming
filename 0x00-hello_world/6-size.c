9fc2f4ef429c68073c57b7ccc5fd2e19d8ea02b6
Ndege <kamothowilliam2@gmail.com>
2023-09-07 12:57:42 -0700

    bytes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,9 +14,9 @@ int main(void)
 	float pi;
 
 	printf("Size of a char: %zu byte(s)\n", sizeof(c));
-	printf("Size of an int: %zu byte (s)\n", sizeof(i));
-	printf("Size of a long int: %zu byte (s)\n", sizeof(l));
-	printf("Size of a long long int: %zu byte (s)\n", sizeof(ll));
-	printf("Size of a float: %zu byte (s)\n", sizeof(pi));
+	printf("Size of an int: %zu byte(s)\n", sizeof(i));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(l));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(ll));
+	printf("Size of a float: %zu byte(s)\n", sizeof(pi));
 	return (0);
 }
