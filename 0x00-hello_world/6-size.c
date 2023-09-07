7065efebbdbe43f3c85e4bb0e0d7b5ebbd4b4eaa
Ndege <kamothowilliam2@gmail.com>
2023-09-07 12:41:25 -0700

    byte sizes of variable types

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+*main - takes in arguments to show their memory size
+*
+*Return: always returns 0 (Success)
+*/
+int main(void)
+{
+	char c;
+	int i;
+	long l;
+	long long ll;
+	float pi;
+
+	printf("The size of a char: %zu byte(s)\n", sizeof(c));
+	printf("The size of an int: %zu byte (s)\n", sizeof(i));
+	printf("The size of a long int: %zu byte (s)\n", sizeof(l));
+	pritnf("The size of a long long int: %zu byte (s)\n", sizeof(ll));
+	printf("The size of a float: %zu byte (s)\n", sizeof(pi));
+	return (0);
+}
