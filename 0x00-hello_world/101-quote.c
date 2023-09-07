262e1c3ac716b6e8912062f731d6e57d274aa4b8
Ndege <kamothowilliam2@gmail.com>
2023-09-07 15:53:17 -0700

    printing

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,8 +8,9 @@
 */
 int main(void)
 {
-	const char *errorMessage = "and that piece of art is useful\" - Dora\
-	Korpar, 2015-10-19\n";
+	const char *errorMessage = "and that piece of art is useful\" - Dora\n"
+	"Korpar, 2015-10-19\n";
+
 
 	write(2, errorMessage, 59);
 	return (1);
