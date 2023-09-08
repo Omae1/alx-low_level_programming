f483f8ef86dcac314835ec0ac3fcbe48ebd387a6
Ndege <kamothowilliam2@gmail.com>
2023-09-08 01:53:17 -0700

    final commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,5 @@
-#include <unistd.h>
 #include <stdio.h>
+#include <unistd.h>
 
 /**
 *main- main function the execution starts here
@@ -8,9 +8,6 @@
 */
 int main(void)
 {
-	const char *errorMessage = "and that piece of art is useful\" - Dora\
-Korpar, 2015-10-19\n";
-
-	write(2, errorMessage, 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59);
 	return (1);
 }
