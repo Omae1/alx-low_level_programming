69b982499bb06e33f9f34f7faeb70f9f62403799
Ndege <kamothowilliam2@gmail.com>
2023-09-12 13:21:05 -0700

    printing last digit using user defined function

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *print_last_digit -prints the last digit of a parameter
+ *
+ *@c: must be a number
+ * Return:  Always 0 (success)
+ */
+int print_last_digit(int c)
+{
+if (c < 0)
+c = -c;
+if (c >= 10 || c <= -10)
+{
+_putchar(c % 10 + 48);
+}
+else
+{
+_putchar(c + 48);
+}
+return (0);
+}
