e595ede2387bd83cfbb57459c5c6d823f26abff8
Ndege <kamothowilliam2@gmail.com>
2023-09-12 07:39:38 -0700

    printing alphabets with user-defined function

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+*print_alphabet - invocation of the function; prints alphabets
+*
+*Return: null (function is void)
+*/
+void print_alphabet(void)
+{
+int i;
+
+for (i = 'a'; i <= 'z'; i++)
+_putchar(i);
+_putchar('\n');
+return;
+}
