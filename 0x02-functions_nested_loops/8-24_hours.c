c27d52a03d4004b3a29b43fa3f0219a3acd9e3d2
Ndege <kamothowilliam2@gmail.com>
2023-09-12 15:33:44 -0700

    printing every minute using user-defined function

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -17,8 +17,8 @@ for (j = 0; j <= 59; j++)
 _putchar(i / 10 + 48);
 _putchar(i % 10 + 48);
 _putchar(':');
-_putchar(j / 10+ 48);
-_putchar(j % 10+ 48);
+_putchar(j / 10 + 48);
+_putchar(j % 10 + 48);
 _putchar('\n');
 }
 }
