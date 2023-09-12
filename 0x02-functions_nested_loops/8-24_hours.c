135730d362ad8f76d9fa79bfe979d2713a1efecf
Ndege <kamothowilliam2@gmail.com>
2023-09-12 15:32:07 -0700

    printing every minute using user-defined function

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ *jack_bauer -prints every minute of 24 hrs
+ *
+ *
+ * Return:  returns nothing function void
+ */
+void jack_bauer(void)
+{
+long i, j;
+
+for (i = 0; i <= 23; i++)
+{
+for (j = 0; j <= 59; j++)
+{
+_putchar(i / 10 + 48);
+_putchar(i % 10 + 48);
+_putchar(':');
+_putchar(j / 10+ 48);
+_putchar(j % 10+ 48);
+_putchar('\n');
+}
+}
+return;
+}
