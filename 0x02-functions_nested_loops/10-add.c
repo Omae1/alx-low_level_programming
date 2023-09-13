bf21506c247348c12082ad76adf4ce564f198e39
Ndege <kamothowilliam2@gmail.com>
2023-09-13 03:28:24 -0700

    user-defined function for adding two numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *add -adds teo int and return result
+ *
+ *@num1:must be a number
+ *@num2:must be a number
+ * Return:  Always 0 (success)
+ */
+int add(int num1, int num2)
+{
+int sum;
+
+sum = num1 + num2;
+return (sum);
+}
