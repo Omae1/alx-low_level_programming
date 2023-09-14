a34a436f782d78e02fab1e80429c10464c1007ce
Ndege <kamothowilliam2@gmail.com>
2023-09-14 14:25:52 -0700

    printing a line on terminal

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -9,12 +9,18 @@
 */
 void print_line(int n)
 {
+if (n <= 0)
+	{
+	_putchar('\n');
+	}
+	else
+	{
+	int i;
 
-
-if (n > 0)
-
-_putchar(95);
-_putchar('\n');
-
-return;
+	for (i = 1; i <= n; i++)
+	{
+	_putchar('_');
+	}
+	_putchar('\n');
+	}
 }
