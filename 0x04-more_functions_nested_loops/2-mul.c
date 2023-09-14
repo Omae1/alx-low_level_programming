7472b97eed8b772d304bb4f199d909cbdcab07b4
Ndege <kamothowilliam2@gmail.com>
2023-09-14 12:32:02 -0700

    user defined functions for multiplying two numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+*mul - multiplies two intergers
+*
+*@a:  argument must be an int
+*
+*@b:  argument must be an int
+*
+*Return: Always 0 (success)
+*/
+int mul(int a, int b)
+{
+int product = (a * b);
+return (product);
+
+return (0);
+}
