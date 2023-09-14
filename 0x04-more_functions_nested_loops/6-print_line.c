3946dd7286c0d918c60c6f62ed237751b49c734b
Ndege <kamothowilliam2@gmail.com>
2023-09-14 14:15:36 -0700

    printing a line on terminal

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+*print_line - prints line on the terminal
+*
+*@n:  argument must be an int
+*
+*
+*/
+void print_line(int n)
+{
+
+
+if (n > 0)
+
+_putchar(95);
+_putchar('\n');
+
+return;
+}
