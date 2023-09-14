174fd174a4a71880bfc66cf9236a0008a4d85ee5
Ndege <kamothowilliam2@gmail.com>
2023-09-14 12:15:27 -0700

    user-defined function for checking if argument is uppercase

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,14 +1,18 @@
 #include "main.h"
 
 /**
-* _isupper- checks if alphabets is upper
+*_isupper - checks if alphabets is upper
 *
-Return: returns 1 if true0 when false
+*@c:  argument must be an int
+*
+*Return: Always 0 (success)
 */
 int _isupper(int c)
 {
 if (c >= 65 && c <= 90)
 return (1);
 else
+return (0);
+
 return (0);
 }
