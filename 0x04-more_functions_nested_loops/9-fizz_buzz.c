f975cf3003580af34b9f4a6277f2ff7ad464c17a
Ndege <kamothowilliam2@gmail.com>
2023-09-15 04:07:55 -0700

    FizzBuzz logic

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+*main - filters out numbers
+*
+*Return: Always 0 (success)
+*
+*
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+	if (i % 3 == 0 && i % 5 == 0)
+	printf("FizzBuzz ");
+	else if (i % 3 == 0)
+	printf(" Fizz ");
+	else if (i % 5 == 0)
+	printf("Buzz");
+	else
+	printf("%d ", i);
+	}
+	printf("\n");
+return (0);
+}
