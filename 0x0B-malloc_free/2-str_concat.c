3b9e541502b2be244c84de1178c1e762ea0c3c86
Ndege <kamothowilliam2@gmail.com>
2023-10-02 07:24:30 -0700

    allocating memory of a concatenated string

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -9,24 +9,25 @@
 */
 char *str_concat(char *s1, char *s2)
 {
-	int i, j, length1 = 0, length2 = 0, totalLength = 0;
+	int i = 0, length1 = 0, length2 = 0, totalLength = 0;
 	char *str;
 
 	if (s1 == NULL || s2 == NULL)
 		return ('\0');
-	for (i = 0; s1[i] != '\0'; i++)
-		length1++;
-	for (i = 0; s2[i] != '\0'; i++)
+
+	while (s2 != NULL && s2[length2] != '\0')
 		length2++;
+	while (s1 != NULL && s1[length1] != '\0')
+		length1++;
 	totalLength = length1 + length2;
 	str = malloc((totalLength + 1) * sizeof(char));
 
 	if (str == NULL)
 		return (NULL);
-	for (i = 0; i < length1; i++)
-		str[i] = s1[i];
-	for (j = 0; j < length2; j++)
-		str[i++] = s2[j];
+	while (s1 != NULL  && *s1 != '\0')
+		str[i++] = *s1++;
+	while (s2 != NULL && *s2 != '\0')
+		str[i++] = *s2++;
 	str[i] = '\0';
 return (str);
 }
