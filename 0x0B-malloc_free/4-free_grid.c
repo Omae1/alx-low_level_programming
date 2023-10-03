92d1882aa8185841b2e2bdc1c1c73e038bfc6674
Ndege <kamothowilliam2@gmail.com>
2023-10-03 12:23:00 -0700

    funiction frees a two dimension array of  interger

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+*free_grid - frees a 2D array of intergers
+*@grid: the 2D array to be freed
+*@height: the height of the grid
+*/
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+		free(grid[i]);
+
+	free(grid);
+}
