a1b2a5660168a72b30f056bb0733dbeec26d2fdb
Ndege <kamothowilliam2@gmail.com>
2023-10-09 05:55:26 -0700

    first struct type

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+
+/**
+ * struct dog - contains dogs name; its age and its ownner
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ */
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+
+
+#endif
