8fc43ec95a5a916c9366bb8d8608496b5edfac9b
Ndege <kamothowilliam2@gmail.com>
2023-10-24 04:24:12 -0700

    function deletes node at position index

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,38 @@
+#include "lists.h"
+
+/**
+*delete_nodeint_at_index - deletes node at position index
+*@head: a linked list
+*@index: node's position
+*Return: 1 if successful and -1 when unsuccessful
+*/
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t *currentNode = *head;
+	listint_t *previousNode = NULL;
+	unsigned int i = 0;
+
+	if (*head == NULL)
+		return (-1);
+
+	if (index == 0)
+	{
+		listint_t *tempNode = *head;
+		*head = (*head)->next;
+		free(tempNode);
+		return (1);
+	}
+
+	for (; i < index && currentNode != NULL; i++)
+	{
+		previousNode = currentNode;
+		currentNode = currentNode->next;
+	}
+
+	if (currentNode == NULL)
+		return (-1);
+
+	previousNode->next = currentNode->next;
+	free(currentNode);
+	return (-1);
+}
