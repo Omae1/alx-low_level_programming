987b55f6be5fb2a4bfb946510f4202322a985a33
Ndege <kamothowilliam2@gmail.com>
2023-10-24 01:33:42 -0700

    fucntion returns the nth node of a linked list

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -10,6 +10,9 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int i;
 
+	if (head == NULL)
+		return (NULL);
+
 	while (i < index && head != NULL)
 	{
 		head = head->next;
