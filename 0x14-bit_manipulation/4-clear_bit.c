f0f268d4e8a7104b1d4ef7ee143e0c4f5357361a
Ndege <kamothowilliam2@gmail.com>
2023-10-26 09:50:24 -0700

    function clears bit at a given index

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+*clear_bit - sets value of bit to zero
+*@n: input value/parameter
+*@index: bit index value to be cleared
+*Return: 1 if success
+*/
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (indx > sizeof(n) * 8)
+		return (-1);
+	*n &= ~(1 << index);
+	return (1);
+}
