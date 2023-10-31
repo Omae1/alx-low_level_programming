d4c97296f64f55583817730ebaed016bd8fa40ab
Ndege <kamothowilliam2@gmail.com>
2023-10-31 00:03:58 -0700

    copies to and from

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,6 +1,8 @@
 #include "main.h"
 #include <stdio.h>
 
+
+
 char *create_bffer(char *fle);
 void close_fle(int fd);
 /**
@@ -10,15 +12,15 @@ void close_fle(int fd);
 */
 char *create_bffer(char *fle)
 {
-	char *buff;
+char *buff;
 
-	buff = malloc(sizeof(char) * 1024);
-	if (buff == NULL)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", fle);
-		exit(99);
-	}
-	return (buff);
+buff = malloc(sizeof(char) * 1024);
+if (buff == NULL)
+{
+dprintf(STDERR_FILENO, "Error: Can't write to %s\n", fle);
+exit(99);
+}
+return (buff);
 }
 
 /**
@@ -27,14 +29,14 @@ char *create_bffer(char *fle)
 */
 void close_fle(int fd)
 {
-	int i;
+int i;
 
-	i = close(fd);
-	if (i == -1)
-	{
-		dprintf(STDERR_FILENO, "Erro: Can't close fd %d\n", fd);
-		exit(100);
-	}
+i = close(fd);
+if (i == -1)
+{
+dprintf(STDERR_FILENO, "Erro: Can't close fd %d\n", fd);
+exit(100);
+}
 
 
 }
@@ -47,43 +49,40 @@ void close_fle(int fd)
 */
 int main(int argc, char *argv[])
 {
-	int f, t, writ, rd;
-	char *buff;
+int f, t, writ, rd;
+char *buff;
 
-	if (argc != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: cpfile_from file_to\n");
-		exit(97);
-	}
-	buff = create_bffer(argv[2]);
-	f = open(argv[1], O_RDONLY);
-	rd = read(f, buff, 1024);
-	t = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	do {
-		if (f == -1 || rd == -1)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-			free(buff);
-			close(f);
-			close(t);
-			exit(98);
-		}
-		writ = write(t, buff, rd);
-		if (t == -1 || writ == -1)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-			free(buff);
-			close_fle(f);
-			close(t);
-			exit(99);
-		}
-		rd = read(f, buff, 1024);
-		t = open(argv[2], O_WRONLY | O_APPEND);
-	} while (rd > 0);
+if (argc != 3)
+{
+dprintf(STDERR_FILENO, "Usage: cpfile_from file_to\n");
+exit(97);
+}
+buff = create_bffer(argv[2]);
+f = open(argv[1], O_RDONLY);
+rd = read(f, buff, 1024);
+umask(0);
+t = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+do {
+if (f == -1 || rd == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+free(buff);
+exit(98);
+}
+writ = write(t, buff, rd);
+if (t == -1 || writ == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+free(buff);
+exit(99);
+}
+rd = read(f, buff, 1024);
+t = open(argv[2], O_WRONLY | O_APPEND);
+} while (rd > 0);
 
-	free(buff);
-	close_fle(f);
-	close_fle(t);
-	return (0);
+free(buff);
+close_fle(f);
+close_fle(t);
+return (0);
 
 }
